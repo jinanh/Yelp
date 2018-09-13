8ef2a2f9ffb521faa81b83f726ea81075c981f6a
jinanh <jhuang27@mail.sfsu.edu>
2018-09-12 20:11:59 -0700

    Initial Commit

---
+++ YelpApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YelpApp
+//
+//  Created by Jinan Huang on 9/12/18.
+//  Copyright © 2018 Jinan Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
